1b37931b13a927224a7d0832a1ed2e7c5817e307
hari080802 <162272034+hari080802@users.noreply.github.com>
2024-03-05 14:35:48 +0530

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,20 +1,9 @@
 angular.module('weatherApp', [])
-    .controller('WeatherController', function($scope, $http) {
-        $scope.getWeather = function() {
-            var apiKey = 'YOUR_API_KEY';
-            var apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-            var location = $scope.location;
-
-            $http.get(apiUrl, {
-                params: {
-                    q: location,
-                    appid: apiKey,
-                    units: 'metric' // Use Celsius
-                }
-            }).then(function(response) {
-                $scope.weatherData = response.data;
-            }).catch(function(error) {
-                $scope.errorMessage = 'Error fetching weather data. Please try again.';
-            });
-        };
-    });
+  .controller('WeatherController', function() {
+    this.city = 'Sample City';
+    this.country = 'Sample Country';
+    this.temperature = '25';
+    this.humidity = '70';
+    this.windSpeed = '3';
+    this.weather = 'Partly Cloudy';
+  });
